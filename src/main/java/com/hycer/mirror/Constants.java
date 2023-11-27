867f553b0bf1b548229c005f130635e185db4513
LanceHE6 <2765543491@qq.com>
2023-11-27 21:00:12 +0800

    refactor.常量接口封装

---
+++ src/main/java/com/hycer/mirror/Constants.java
@@ -0,0 +1,9 @@
+package com.hycer.mirror;
+
+public interface Constants {
+    String BACKUP_PATH = "MirrorBackup\\";
+    String WORLD_PATH = "world";
+    String CONFIG_PATH = "config/";
+    String CONFIG_FILE = "Mirror.json";
+    String RETREAT_BAT_FILE = "retreat.bat";
+}
